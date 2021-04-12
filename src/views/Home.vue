4d82c3b80bb288f3777da3343b81de8ff16547a3
Manuel Quarneti <manuelquarneti@gmail.com>
2021-04-12 15:01:35 +0200

    select version

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,15 +10,21 @@ const updateVersions = () => {
 
 const isVersionSelectorDropdownOpen = ref(false)
 
+const selectedVersion = ref<Version>({ id: 'Select version ↓', url: '' })
+const selectVersion = (version: Version) => {
+  selectedVersion.value = version
+  isVersionSelectorDropdownOpen.value = false
+}
+
 onMounted(updateVersions)
 </script>
 
 <template>
   <h1 class="text-5xl text-center">runmc</h1>
   <div class="my-8">
-    <button class="border-2 w-40 rounded-l-full py-2 px-4" @click="isVersionSelectorDropdownOpen = !isVersionSelectorDropdownOpen">Select version ↓</button>
+    <button class="border-2 w-40 rounded-l-full py-2 px-4" @click="isVersionSelectorDropdownOpen = !isVersionSelectorDropdownOpen">{{ selectedVersion.id }}</button>
     <div class="absolute border-2 rounded-3xl mt-2 w-40 flex flex-col items-center divide-y" v-show="isVersionSelectorDropdownOpen">
-      <button class="p-2 w-full" v-for="version in versions" :key="version.id">{{ version.id }}</button>
+      <button class="p-2 w-full" v-for="version in versions" :key="version.id" @click="selectVersion(version)">{{ version.id }}</button>
       <router-link class="p-2 w-full text-center" to="/versions">+</router-link>
     </div>
     <button class="text-white bg-blue-500 border-2 border-blue-500 rounded-r-full py-2 px-4">▶</button>
