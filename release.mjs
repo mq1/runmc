cf79d416509c008b74320f70da71890e21c217c2
Manuel Quarneti <manuelquarneti@gmail.com>
2021-04-24 19:12:54 +0200

    disable signature in release.json

--- release.mjs
+++ release.mjs
@@ -3,7 +3,6 @@ import packageConfig from './package.json'
 
 const version = packageConfig.version
 const dateTime = new Date()
-const signature = 'dW50cnVzdGVkIGNvbW1lbnQ6IG1pbmlzaWduIHB1YmxpYyBrZXk6IDFEMDU4RTQ5Rjg4QzUwQzkKUldUSlVJejRTWTRGSFY1VVBhSGhYT2lZSXZQMndJYk81aWI1SHJVMGxMM0dBd0VEYThpai8zcTIK'
 
 const json = {
   name: version,
@@ -11,15 +10,15 @@ const json = {
   pub_date: dateTime.toISOString(),
   platforms: {
     darwin: {
-      signature,
+      signature: '',
       url: `https://github.com/mq1/runmc/releases/download/${version}/runmc_${version}_x64.app.tar.gz`,
     },
     linux: {
-      signature,
+      signature: '',
       url: `https://github.com/mq1/runmc/releases/download/${version}/runmc_${version}_amd64.AppImage.tar.gz`,
     },
     win64: {
-      signature,
+      signature: '',
       url: `https://github.com/mq1/runmc/releases/download/${version}/runmc_${version}_x64.msi.zip`,
     },
   },
