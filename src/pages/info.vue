79c278988227393d19e23dc489c4105a602a80d4
Manuel Quarneti <manuelquarneti@gmail.com>
2021-05-22 13:22:00 +0200

    fix footer

--- src/pages/info.vue
+++ src/pages/info.vue
@@ -16,9 +16,6 @@ const { t } = useI18n()
     <div class="px-2 flex items-center">
       © 2021 Manuel Quarneti
     </div>
-    <div class="px-2 flex items-center">
-      Hosted on Netlify
-    </div>
     <a class="px-2 flex items-center" href="https://github.com/mq1/mq1.eu">
       <carbon-logo-github />
     </a>
