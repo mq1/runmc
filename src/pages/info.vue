07cb98782179b44e0ca6b8b9d82e0f8906573fd3
Manuel Quarneti <manuelquarneti@gmail.com>
2021-05-22 13:20:52 +0200

    new footer on info page

--- src/pages/info.vue
+++ src/pages/info.vue
@@ -9,9 +9,18 @@ const { t } = useI18n()
   <h1 class="text-2xl">
     {{ t('info.version', { version }) }}
   </h1>
-  <div>
-    MIT Licensed |
-    © 2021 Manuel Quarneti |
-    {{ t('info.sourcecode') }} <a href="https://github.com/mq1/runmc" class="underline">{{ t('info.here') }}</a>
-  </div>
+  <footer class="flex justify-center divide-x-2">
+    <div class="px-2 flex items-center">
+      MIT Licensed
+    </div>
+    <div class="px-2 flex items-center">
+      © 2021 Manuel Quarneti
+    </div>
+    <div class="px-2 flex items-center">
+      Hosted on Netlify
+    </div>
+    <a class="px-2 flex items-center" href="https://github.com/mq1/mq1.eu">
+      <carbon-logo-github />
+    </a>
+  </footer>
 </template>
