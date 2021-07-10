2b442a6f1f5fb5c0ba29bc845ce2e4cbf0a75bad
Manuel Quarneti <manuelquarneti@gmail.com>
2021-07-10 12:51:58 +0200

    update components defs

--- components.d.ts
+++ components.d.ts
@@ -3,6 +3,23 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
+    CarbonArrowRight: typeof import('virtual:vite-icons/carbon/arrow-right')['default']
+    CarbonBadge: typeof import('virtual:vite-icons/carbon/badge')['default']
+    CarbonCheckmark: typeof import('virtual:vite-icons/carbon/checkmark')['default']
+    CarbonDownload: typeof import('virtual:vite-icons/carbon/download')['default']
+    CarbonFire: typeof import('virtual:vite-icons/carbon/fire')['default']
+    CarbonHome: typeof import('virtual:vite-icons/carbon/home')['default']
+    CarbonInformation: typeof import('virtual:vite-icons/carbon/information')['default']
+    CarbonLayers: typeof import('virtual:vite-icons/carbon/layers')['default']
+    CarbonLogoGithub: typeof import('virtual:vite-icons/carbon/logo-github')['default']
+    CarbonPen: typeof import('virtual:vite-icons/carbon/pen')['default']
+    CarbonReset: typeof import('virtual:vite-icons/carbon/reset')['default']
+    CarbonRestart: typeof import('virtual:vite-icons/carbon/restart')['default']
+    CarbonSave: typeof import('virtual:vite-icons/carbon/save')['default']
+    CarbonSettings: typeof import('virtual:vite-icons/carbon/settings')['default']
+    CarbonSettingsAdjust: typeof import('virtual:vite-icons/carbon/settings-adjust')['default']
+    CarbonTrashCan: typeof import('virtual:vite-icons/carbon/trash-can')['default']
+    CarbonUserMultiple: typeof import('virtual:vite-icons/carbon/user-multiple')['default']
     Instance: typeof import('./src/components/instance.vue')['default']
     NavLink: typeof import('./src/components/nav-link.vue')['default']
   }
