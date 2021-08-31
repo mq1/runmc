660a1ece3bded56f0285ddfd1770c0fccd202048
Manuel Quarneti <manuelquarneti@gmail.com>
2021-08-31 10:28:24 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import WindiCSS from 'vite-plugin-windicss'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
-import Components from 'vite-plugin-components'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
+import ViteComponents from 'vite-plugin-components'
+import WindiCSS from 'vite-plugin-windicss'
+import AutoImport from 'unplugin-auto-import/vite'
 import VueI18n from '@intlify/vite-plugin-vue-i18n'
 
 // https://vitejs.dev/config/
@@ -19,20 +20,48 @@ export default defineConfig({
 
   plugins: [
     Vue(),
+
+    // https://github.com/hannoeru/vite-plugin-pages
     Pages(),
+
+    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
     Layouts(),
-    WindiCSS(),
-    Components({
-      globalComponentsDeclaration: true,
-      customComponentResolvers: IconsResolver({
-        componentPrefix: '',
-        enabledCollections: ['carbon'],
-      }),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        '@vueuse/head',
+        '@vueuse/core',
+      ],
     }),
+
+    ViteComponents({
+      // generate `components.d.ts` for ts support with Volar
+      globalComponentsDeclaration: true,
+
+      // auto import icons
+      customComponentResolvers: [
+        // https://github.com/antfu/unplugin-icons
+        IconsResolver({
+          componentPrefix: '',
+          enabledCollections: ['carbon'],
+        }),
+      ],
+    }),
+
+    // https://github.com/antfu/unplugin-icons
     Icons(),
+
+    // https://github.com/antfu/vite-plugin-windicss
+    WindiCSS(),
 
     // https://github.com/intlify/vite-plugin-vue-i18n
     VueI18n({
+      runtimeOnly: true,
+      compositionOnly: true,
       include: [path.resolve(__dirname, 'locales/**')],
     }),
   ],
