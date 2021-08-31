c1f6cd8f4df8c0b276d5704eddf8136f8b1cc248
Manuel Quarneti <manuelquarneti@gmail.com>
2021-08-31 12:06:49 +0200

    use vite-plugin-tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,23 @@
   windows_subsystem = "windows"
 )]
 
-mod cmd;
+use std::{io, fs::File};
+
+#[tauri::command]
+async fn wget(url: String, path: String) -> Result<(), String> {
+  let resp = reqwest::get(url).await.expect("request failed");
+  let bytes = resp.bytes().await.expect("failed reading content");
+  let mut bytes = bytes.as_ref();
+  let mut out = File::create(path).expect("failed to create file");
+
+  io::copy(&mut bytes, &mut out).expect("failed to copy content");
+
+  Ok(())
+}
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![
-      cmd::wget
-    ])
+    .invoke_handler(tauri::generate_handler![wget])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
