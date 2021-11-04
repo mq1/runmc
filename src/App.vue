903d27090ac43b8b7263513617b85e1bef472e38
Manuel Quarneti <manuelquarneti@gmail.com>
2021-11-04 10:38:26 +0100

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,15 @@ import { read } from '~/logic/config'
 const { locale } = useI18n()
 
 onBeforeMount(async() => {
+  // create appDir if not present
   const d = await appDir()
   await createDir(d, { recursive: true })
+  console.log({ appDir: d })
 
+  // set locale
   const config = await read()
   locale.value = config.locale
+  console.log({ locale: locale.value })
 })
 </script>
 
